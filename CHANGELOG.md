303e395c4edd141b89112d97ccf3a47dba387f8d
PC-CASA\Nicola <nicolariva@live.it>
2020-02-27 16:49:57 +0100

    Aggiunta del prototipo della mappa di login

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Simple CLub Manager
+
+## Unreleased
+
+### Added
+
+- Lavoro sul branch feature/auth per la creazione della mappa di login
+- Aggiunto Material (ng add @angular/material)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { environment } from '../environments/environment';
+import { LoginComponent } from './admin/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig)
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
